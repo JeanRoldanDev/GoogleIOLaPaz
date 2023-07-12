03c35fc2495210d2213a5ad58490d87f8501d5e0
JeanRoldanDev <jeancarlos_2020@hotmail.com>
2023-07-12 02:15:43 -0500

    impl MultiRepositoryProvider

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:googleiolapaz/core/ia/ia.dart';
+import 'package:googleiolapaz/core/ia/ia_impl.dart';
+import 'package:googleiolapaz/core/mqtt/mqtt.dart';
+import 'package:googleiolapaz/core/mqtt/mqtt_impl.dart';
 import 'package:googleiolapaz/layouts/layouts.dart';
 import 'package:googleiolapaz/page/splash/splash_page.dart';
 
@@ -7,16 +12,26 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        fontFamily: CFonst.jetbrainsmono,
-        colorScheme: ColorScheme.fromSwatch(
-          backgroundColor: const Color.fromARGB(255, 234, 236, 238),
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider<IA>(
+          create: (context) => IAimpl(),
         ),
+        RepositoryProvider<Mqtt>(
+          create: (context) => MqttImpl(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          useMaterial3: true,
+          fontFamily: CFonst.jetbrainsmono,
+          colorScheme: ColorScheme.fromSwatch(
+            backgroundColor: const Color.fromARGB(255, 234, 236, 238),
+          ),
+        ),
+        home: const SplashPage(),
       ),
-      home: const SplashPage(),
     );
   }
 }
